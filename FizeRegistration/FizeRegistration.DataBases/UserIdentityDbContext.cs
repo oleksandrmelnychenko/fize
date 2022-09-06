305494af771908ef6e73756678a4b4baccde2e7e
balis77 <106303285+balis77@users.noreply.github.com>
2022-09-06 18:39:33 +0300

    Visual Fix and Add icons

--- FizeRegistration/FizeRegistration.DataBases/UserIdentityDbContext.cs
+++ FizeRegistration/FizeRegistration.DataBases/UserIdentityDbContext.cs
@@ -20,7 +20,7 @@ public class UserIdentityDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=tcp:fizeserver.database.windows.net,1433;Initial Catalog=fizedata;Persist Security Info=False;User ID=balis77;Password=Nspre169169!;");
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Fize;");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
